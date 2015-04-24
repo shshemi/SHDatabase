e054d627b74cb73714d3c44f9c33258dffc0c609
Shayan Hashemi <shshemi@gmail.com>
2015-04-24 22:32:20 +0430

    Init files added

---
+++ SHDatabaseTest/SHDatabaseTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SHDatabaseTest
+//
+//  Created by Shayan on 4/24/15.
+//  Copyright (c) 2015 Shayan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SHDatabaseTest/SHDatabaseTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SHDatabaseTest
+//
+//  Created by Shayan on 4/24/15.
+//  Copyright (c) 2015 Shayan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
