0977e7a3792f67f1d36c967023a4aa148878214d
Shayan Hashemi <shshemi@gmail.com>
2015-04-25 18:38:00 +0430

    commit

---
+++ UITableViewBounce/UITableViewBounce/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewBounce
+//
+//  Created by Shayan on 4/25/15.
+//  Copyright (c) 2015 Shayan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewBounce/UITableViewBounce/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  UITableViewBounce
+//
+//  Created by Shayan on 4/25/15.
+//  Copyright (c) 2015 Shayan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
